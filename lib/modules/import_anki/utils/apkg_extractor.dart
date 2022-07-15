19e52e02c9da621156d078ac73689b77318e9b70
friebetill <friebetill@gmail.com>
2022-07-15 13:45:15 +0200

    Handle cloze cards during Anki import

--- lib/modules/import_anki/utils/apkg_extractor.dart
+++ lib/modules/import_anki/utils/apkg_extractor.dart
@@ -86,22 +86,38 @@ Future<List<AnkiCard>> _extractCards(
   final textCleaner = AnkiTextCleaner();
 
   final cards = <AnkiCard>[];
-  for (final row in cardsJoinsNotesTable) {
-    final noteType = ankiNoteTypes.singleWhere((n) => n.id == row['mid']);
-    var htmlFront = noteType.tmpls[row['ord']! as int]['qfmt'] as String;
-    var htmlBack = noteType.tmpls[row['ord']! as int]['afmt'] as String;
+  for (final cardRow in cardsJoinsNotesTable) {
+    final noteType = ankiNoteTypes.singleWhere((n) => n.id == cardRow['mid']);
+
+    String htmlFront;
+    String htmlBack;
+    if (noteType.isCloze) {
+      htmlFront = (noteType.tmpls[0]['qfmt'] as String).replaceFirst(
+        '{{cloze:Text}}',
+        cardRow['flds']! as String,
+      );
+      htmlBack = (noteType.tmpls[0]['afmt'] as String).replaceFirst(
+        '{{cloze:Text}}',
+        cardRow['flds']! as String,
+      );
+    } else {
+      htmlFront = noteType.tmpls[cardRow['ord']! as int]['qfmt'] as String;
+      htmlBack = noteType.tmpls[cardRow['ord']! as int]['afmt'] as String;
+    }
 
     htmlFront = textCleaner.replaceFieldsWithContent(
       cardText: htmlFront,
-      cardEntry: row,
+      cardRow: cardRow,
       noteType: noteType,
       decks: decks,
+      isFront: true,
     );
     htmlBack = textCleaner.replaceFieldsWithContent(
       cardText: htmlBack,
-      cardEntry: row,
+      cardRow: cardRow,
       noteType: noteType,
       decks: decks,
+      isFront: false,
       ignoreFields: ['Front'],
     );
 
@@ -109,7 +125,7 @@ Future<List<AnkiCard>> _extractCards(
     htmlBack = textCleaner.removeVideoAudioLinks(htmlBack);
 
     cards.add(AnkiCard(
-      deckId: row['did']! as int,
+      deckId: cardRow['did']! as int,
       front: htmlFront,
       back: htmlBack,
     ));
